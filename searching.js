5f837b561027e1d7dd7806e36cf2f0c078a501e1
danish-1234 <106437392+danish-1234@users.noreply.github.com>
2023-01-26 17:51:27 +0530

    Add files via upload

---
+++ searching.js
@@ -0,0 +1,61 @@
+function openPage()
+        {
+            var x = document.getElementById("vethuku").value;
+
+            if (x === "art")
+            {
+                window.open("rings.html");
+            }
+            if (x === "bags")
+            {
+                window.open("rings.html");
+            }
+            if (x === "bangles")
+            {
+                window.open("rings.html");
+            }
+            if (x === "candles")
+            {
+                window.open("rings.html");
+            }
+            if (x === "chains")
+            {
+                window.open("rings.html");
+            }
+            if (x === "decoratives")
+            {
+                window.open("rings.html");
+            }
+            if (x === "earrings")
+            {
+                window.open("rings.html");
+            }
+            if (x === "gifts")
+            {
+                window.open("rings.html");
+            }
+            if (x === "handloom sarees")
+            {
+                window.open("rings.html");
+            }
+            if (x === "keychains")
+            {
+                window.open("rings.html");
+            }
+            if (x === "phonecases")
+            {
+                window.open("rings.html");
+            }
+            if (x === "rings")
+            {
+                window.open("rings.html");
+            }
+            if (x === "t shirts")
+            {
+                window.open("rings.html");
+            }
+            else
+            {
+                alert("PRODUCT NOT FOUND !!!...")
+            }
+        }
